8cf16292707d1cef360f9a76e5b9e80801aa3988
Sizan75 <sakiuzzaman26@gmail.com>
2022-09-03 10:33:43 +0600

    card loaded from navigation done

--- js/categories.js
+++ js/categories.js
@@ -27,8 +27,33 @@ const showNewsFeed = (id) =>{
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayNewsFeed(data.data))
 }
 
-
+const displayNewsFeed = data =>{
+    const newsFeedDiv = document.getElementById('news-feed');
+    newsFeedDiv.innerHTML = ''
+    data.forEach(news => {
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('card');
+        cardDiv.classList.add('mb-3');
+        cardDiv.innerHTML = `
+        
+                <div class="row g-0">
+                  <div class="col-md-4">
+                    <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">${news.title}</h5>
+                      <p class="card-text">${news.details}</p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+                </div>
+              
+        `
+        newsFeedDiv.appendChild(cardDiv);
+    });
+}
 
