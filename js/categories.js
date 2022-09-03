1071a573ced715f757d48e032a922f77f322b103
Sizan75 <sakiuzzaman26@gmail.com>
2022-09-03 12:20:45 +0600

    author name,image,date,view,detailsbutton added

--- js/categories.js
+++ js/categories.js
@@ -12,10 +12,10 @@ const displayCategoryName = (categories) =>{
         const categoryLi = document.createElement('li');
         categoryLi.classList.add('nav-item');
         categoryLi.classList.add('me-4');
-        categoryLi.classList.add('ms-5');
+        categoryLi.classList.add('ms-4');
         categoryLi.classList.add('fw-semibold');
         categoryLi.innerHTML = `
-        <a class="nav-link active fw-semibold" aria-current="page" onclick="showNewsFeed('${category.category_id}')" >${category.category_name}</a
+        <button class="btn btn-light"  onclick="showNewsFeed('${category.category_id}')" >${category.category_name}</button>
         `    
         categoriesUl.appendChild(categoryLi);    
     });
@@ -41,13 +41,24 @@ const displayNewsFeed = data =>{
         
                 <div class="row g-0">
                   <div class="col-md-4">
-                    <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                    <img src="${news.thumbnail_url}" class="img-fluid rounded-start"  alt="...">
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
-                      <h5 class="card-title">${news.title}</h5>
-                      <p class="card-text">${news.details}</p>
-                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                      <h5 class="card-title">${news.title ? news.title: "Not Found"}</h5>
+                      <p class="card-text">${news.details ? news.details.slice(0,300) : "no news details found"}...</p>
+                      <div class="d-flex mt-3 justify-content-between me-4 align-items-center">
+                      <div class="d-flex ">
+                       <img src="${news.author.img ? news.author.img : "Author img not found"}" class="rounded-circle" alt="..." width="42" height="42">
+                      <div >
+                        <h6>${news.author.name ? news.author.name : "No aouthor name found"}</h6>
+                        <p>${news.author.published_date ? news.author.published_date.slice(0,10) : "publish date not found"}</p>
+                      </div>
+                      </div>
+                
+                      <h6><i class="fa-sharp fa-solid fa-eye"></i> ${news.total_view ? news.total_view  : "No View"}M</h6>
+                      <button class="btn btn-primary " onclick="">Details</button>
+                    </div>
                     </div>
                   </div>
                 </div>
